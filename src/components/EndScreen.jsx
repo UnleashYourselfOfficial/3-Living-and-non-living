de9ef24f6468ca9c5a8a46cfd50b025b19781157
Evie Frye <msmanika763348@gmail.com>
2026-05-16 19:16:19 +0530

    feat: add winner sound effect to EndScreen component

--- src/components/EndScreen.jsx
+++ src/components/EndScreen.jsx
@@ -1,11 +1,36 @@
-import GreatJob from '../assets/greatjob1.webp';
+import { useEffect, useRef } from "react";
+import GreatJob from "../assets/greatjob1.webp";
+
 function EndScreen({ stars }) {
+
+  const winSound = useRef(null);
+
+  useEffect(() => {
+    if (winSound.current) {
+      winSound.current.currentTime = 0;
+
+      winSound.current
+        .play()
+        .catch(() => {});
+    }
+  }, []);
+
   return (
     <div className="screen">
 
+      <audio
+        ref={winSound}
+        src="/audio/winner.mp3"
+        preload="auto"
+      />
+
       <div className="end-box">
-        <img src={GreatJob} alt="Great Job!" className="end-image" />
-       </div>
+        <img
+          src={GreatJob}
+          alt="Great Job!"
+          className="end-image"
+        />
+      </div>
 
     </div>
   );
